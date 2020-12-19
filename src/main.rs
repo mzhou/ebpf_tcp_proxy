3f1b1b548246a7ffe15d969a5f6087348863c987
Michael Zhou <mzhou@cse.unsw.edu.au>
2020-12-19 16:28:00 +1100

    try loading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use redbpf::load::Loader;
+use std::fmt;
 
-fn main() {
-    let mut loader = Loader::load_file("iotop.elf").expect("error loading probe");
+use redbpf::load::{Loader, LoaderError};
 
-    // attach all the kprobes defined in iotop.elf
-    for kprobe in loader.kprobes_mut() {
-        kprobe
-            .attach_kprobe(&kprobe.name(), 0)
-            .expect(&format!("error attaching program {}", kprobe.name()));
+#[derive(Debug)]
+enum MainError {
+    Loader(LoaderError),
+}
+
+impl fmt::Display for MainError {
+    fn fmt(self: &Self, f: &mut fmt::Formatter) -> fmt::Result {
+        use MainError::*;
+        write!(
+            f,
+            "{}",
+            match self {
+                Loader(e) => format!("loader {:?}", e),
+            }
+        )
     }
+}
+
+fn main() -> Result<(), MainError> {
+    let sockmap_parser_elf =
+        include_bytes!("../target/bpf/programs/sockmap_parser/sockmap_parser.elf");
+    println!("elf is {} bytes", sockmap_parser_elf.len());
+
+    let mut loader = Loader::load(sockmap_parser_elf).map_err(MainError::Loader)?;
+    for socket_filter in loader.socket_filters_mut() {
+        println!("found socket_filter");
+    }
+
+    Ok(())
 }
