6ec0d8f26bb901065f241ac6f39eff843acc38d6
Michael Zhou <mzhou@cse.unsw.edu.au>
2020-12-19 18:29:21 +1100

    now successfully echoes!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::fmt;
+use std::io::Read;
+use std::net::TcpListener;
+use std::os::unix::io::AsRawFd;
 
 use bpf_sys::{
     bpf_attach_type_BPF_SK_SKB_STREAM_PARSER, bpf_attach_type_BPF_SK_SKB_STREAM_VERDICT,
 };
 use redbpf::load::{Loader, LoaderError};
+use redbpf::HashMap;
 
 #[derive(Debug)]
 enum MainError {
+    Accept(std::io::Error),
     AttachMap(redbpf::Error),
+    Bind(std::io::Error),
     Loader(LoaderError),
+    MapCast,
     Map,
 }
 
@@ -19,9 +26,12 @@ impl fmt::Display for MainError {
             f,
             "{}",
             match self {
+                Accept(e) => format!("accept {}", e),
                 AttachMap(e) => format!("attach map {:?}", e),
+                Bind(e) => format!("bind {}", e),
                 Loader(e) => format!("loader {:?}", e),
                 Map => format!("map not found in ebpf program"),
+                MapCast => format!("map key/value type mismatch"),
             }
         )
     }
@@ -49,6 +59,21 @@ fn main() -> Result<(), MainError> {
             )
             .map_err(MainError::AttachMap)?;
     }
+    println!("bpf init done");
 
-    Ok(())
+    // should be SockMap, but api is the same as HashMap, so use it for now
+    let sock_map = HashMap::<i32, i32>::new(map).map_err(|_| MainError::MapCast)?;
+    let ss = TcpListener::bind("[::]:1234").map_err(MainError::Bind)?;
+    loop {
+        let (mut s, a) = ss.accept().map_err(MainError::Accept)?;
+        println!("accepted from {}", a);
+        let key = 0;
+        let val = s.as_raw_fd();
+        sock_map.set(key, val);
+        println!("socket added to map");
+        // wait for close
+        let mut dummy_buf = [0u8; 1];
+        let read_result = s.read(&mut dummy_buf);
+        println!("read has returned {:?}", read_result);
+    }
 }
